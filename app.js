69a711ab6961bd97aa66832606f717e970a76952
Denva <emsley3684@gmail.com>
2021-11-23 11:41:41 -0500

    added npm inquirer as dependancy

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const fs = require('fs');
-
+const inquirer = require("inquirer");
+/*const fs = require('fs');
 const generatePage = require("./src/page-template.js")
 
-const profileDataArgs = process.argv.slice(2);
+const pageHTML = generatePage(names, github);
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
+fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output!');
-});
+});*/
+
+inquirer.prompt ([
+  {
+    name: 'name',
+    message: 'What is your name?',
+  },
+]).then(answers => console.log(answers));
